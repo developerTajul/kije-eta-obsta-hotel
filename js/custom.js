dd02243b56baa969f9686dcaa68a0236f5d9be98
tanvirmadmax <tanvirmadmax@gmail.com>
2024-10-18 07:02:32 +0600

    add: pause on blog page

--- js/custom.js
+++ js/custom.js
@@ -719,6 +719,25 @@
         "<button type='button' class='tab-btn next-btn'><i class='fa fa-arrow-right'></i></button>",
     });
     /*
+       Post Gallery Slider
+       ============================*/
+    $('.post-gallery').slick({
+      slidesToShow: 1,
+      infinite: true,
+      autoplay: true,
+      draggable: true,
+      arrows: true,
+      slidesToScroll: 1,
+      loop: true,
+      dots: false,
+      speed: 300,
+      rtl: false,
+      prevArrow:
+        "<button type='button' class='post-gallery-btn prev-btn'><i class='fa fa-arrow-left'></i></button>",
+      nextArrow:
+        "<button type='button' class='post-gallery-btn next-btn'><i class='fa fa-arrow-right'></i></button>",
+    });
+    /*
        Portfolio Logo Slider
        ============================*/
     $('.portfolio-slider').slick({
